3d1395fa5c9819566bef9826c10e0ac49ac24bb7
FedeClaudi <federicoclaudi@protonmail.com>
2021-10-16 10:55:37 +0100

    bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name="tpd",
-    version="1.8",
+    version="1.8.1",
     description="Easy saving of logs, data and plots",
     long_description=long_description,
     long_description_content_type="text/markdown",
